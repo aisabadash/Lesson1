4cfd9172dcc377de90d5c3a6be3fc0b59471d181
Anna Sabadash <bogatyrchuk@gmail.com>
2024-11-10 19:21:14 +0100

    Completed

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -5,42 +5,58 @@
 // Скопіюйте значення зі змінної name в city.
 // Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
 
-let name;
-let city;
+// let name;
+// let city;
 
-name = "Іван";
-city = name;
- 
+// name = "Іван";
+// city = name;
+
+// console.log(city);
 
 //***2***
 //Який буде результат виконання скрипта?
 // let name = "Olga";
-// console.log(`привіт ${1}`); //
-// console.log(`привіт ${"name"}`); //
-// console.log(`привіт ${name}`); // ?
+// console.log(`привіт ${1}`); // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); // привіт Olga
 
 //***3***
 //Видобути число зі змінних
 // let a = "5";
 // let b = "13cvb";
 // let c = "12.9sxdcfgv";
+
+// a = Number(a);
+// b = Number.parseInt(b);
+// c = Number.parseFloat(c);
+
 // вивести в консоль тип
+// console.log(`${a} (${typeof Number(a)})`);
+// console.log(`${b} (${typeof Number.parseInt(b)})`);
+// console.log(`${c} (${typeof Number.parseFloat(c)})`);
 
 //***4***
 //Зробіть, щоб 0.1 + 0.2 = 0.3
 
+// let sum = (0.1 * 10 + 0.2 * 10) / 10;
+// console.log(sum);
+
 //***5**
 //Поверніть найбільше число с набору 20, 10, 50, 40
+// console.log(Math.max(20, 10, 50, 40));
 
 //***6**
 //Поверніть випадкове число в діапазоні від 2 до 4
+// console.log(Math.random() * (4 - 2) + 2);
 
 //***7**
 //дізнатись довжину message
 // const message = "Welcome to Bahamas!";
+// console.log(message.length);
 
 //***8**
 //вивести в консоль message  великими літерами
+// console.log(message.toUpperCase());
 
 //***9**
 // створити пустий об*єкт
@@ -50,5 +66,18 @@ city = name;
 // додати буль з ключем: like flowers
 // вивести результат в консоль
 
+// const user = {};
+// user.name = "Anna";
+// user.age = 38;
+// user.city = "Kyiv";
+// console.log(user);
+
+// delete user.city;
+// user["like flowers"] = true;
+// console.log(user);
+
 //***10**
 // За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+// for (const key in user) {
+//    console.log(`${key} -> ${user[key]}`);
+// }
